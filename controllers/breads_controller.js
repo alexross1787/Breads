eafbf34abc4ec894f22565a01353e205d9e7860b
alexross1787 <alexross1787@gmail.com>
2024-02-07 19:28:27 -0800

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -16,7 +16,22 @@ breads.get('/', (req, res) => {
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-  res.send(Bread[req.params.arrayIndex])
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
+})
+
+
+
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
 })
 
 
